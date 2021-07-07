dd8249b7e4f504a39c95274655c0e97e847485d1
Mark Broihier <mbroihier@yahoo.com>
2021-07-07 18:20:31 +0000

    2nd pass servo control

--- HW.py
+++ HW.py
@@ -1,40 +1,22 @@
 #!/usr/bin/python3
+
 import os
-import select
-import sys
-import wiringpi
 
 class HW():
     '''
     Class that creates a hardware interface to control a Parallax servo
     '''
-    def __init__(self, timeLow, timeHigh):
+    def __init__(self):
         '''
-        Initialize wiringPi and objects needed
+        Initialize objects needed
         '''
-        self.HW_CLOCK = 19200000.0
-        if os.getuid() == 0:
-            wiringpi.wiringPiSetupGpio()
-            self.PWMOutputPin = 18
-            wiringpi.pinMode(self.PWMOutputPin, wiringpi.PWM_OUTPUT)
-            wiringpi.pwmSetMode(wiringpi.PWM_MODE_MS)
-            period = (timeLow + timeHigh) / 1000000.0
-            self.clock = 200;
-            wiringpi.pwmSetClock(self.clock)
-            self.range_ = int(period * self.HW_CLOCK / self.clock)
-            wiringpi.pwmSetRange(self.range_)
-            self.countsPerMicrosecond = self.HW_CLOCK / (self.clock * 1000000.0)
-            self.softStop0 = int(self.range_ * 0.001 / 0.021) + 2
-            self.softStop1 = int(self.range_ * 0.002 / 0.022) - 2
-            self.setPosition(1500)
-            print("Servo settings:")
-            print("clock: {}, range: {}, countsPerMicrosecond {}".format(self.clock,
-                                                                         self.range_, self.countsPerMicrosecond))
-            print("softStop0: {}, softStop1 {}".format(self.softStop0, self.softStop1))
-            self.servoBusy = False
-        else:
-            print("This object, built with the HW class, requires root privileges")
-            sys.exit(-1)
+        self.position = 1500
+        self.softStop0 = 1100
+        self.softStop1 = 1900
+        self.servoBusy = False
+        self.servoFifo = open("/dev/servo_fifo", "w")
+        self.fdServoFifo = self.servoFifo.fileno()
+        self.setPosition(1500)
 
     def isBusy(self):
         '''
@@ -42,20 +24,16 @@ class HW():
         '''
         return(self.servoBusy)
 
-    def turnOffPWMPin(self):
-        '''
-        turnOffPWMPin - zero the PWM setting to disable pulses
-        '''
-        wiringpi.pwmWrite(self.PWMOutputPin, 0)
-
     def setPosition(self, timeUSec):
         '''
         setPosition - set the servo to an allowed position
         '''
         self.servoBusy = True
-        setting = int(timeUSec * self.countsPerMicrosecond)
+        setting = timeUSec
         if setting >= self.softStop0 and setting <= self.softStop1:
-            wiringpi.pwmWrite(self.PWMOutputPin, setting)
+            self.servoFifo.write("1, {}\n".format(timeUSec))
+            self.servoFifo.write("9, {}\n".format(timeUSec))
+            self.servoFifo.flush()
             self.position = timeUSec
         else:
             print("{} useconds (a setting of {} counts) is beyond the soft stops of the servo".format(timeUSec, setting))
@@ -66,8 +44,8 @@ class HW():
         setPositionFromTo - move servo from position to position
         '''
         self.servoBusy = True
-        setting = int(fromTimeUSec * self.countsPerMicrosecond)
-        toSetting = int(toTimeUSec * self.countsPerMicrosecond)
+        setting = fromTimeUSec
+        toSetting = toTimeUSec
         if setting < self.softStop0:
             setting = self.softStop0
         else:
@@ -75,33 +53,39 @@ class HW():
                 setting = self.softStop1
         if toSetting < self.softStop0:
             toSetting = self.softStop0
-            toTimeUSec = int(toSetting / self.countsPerMicrosecond)
+            toTimeUSec = toSetting
         else:
             if toSetting > self.softStop1:
                 toSetting = self.softStop1
-                toTimeUSec = int(toSetting / self.countsPerMicrosecond)
+                toTimeUSec = toSetting
         tick = 0
-        delta = -1
-        total = setting - toSetting
-        if toSetting > setting:
-            delta = 1
-            total = toSetting - setting
-        interval0 = total >> 2
-        interval1 = interval0 + interval0 + interval0
+        #delta = -1
+        delta = toSetting - setting
+        #total = setting - toSetting + 1
+        total = 2
+        #if toSetting > setting:
+            #delta = 1
+            #total = toSetting - setting + 1  # always go to initial position first (repeats last)
+        print("Going from {} to {} over {} intervals".format(fromTimeUSec, toTimeUSec, total))
         while tick < total:
-            if tick < interval0:
-                wiringpi.pwmWrite(self.PWMOutputPin, setting)
-                select.select([], [], [], 0.020)
-            else:
-                if tick < interval1:
-                    wiringpi.pwmWrite(self.PWMOutputPin, setting)
-                else:
-                    wiringpi.pwmWrite(self.PWMOutputPin, setting)
-                    select.select([], [], [], 0.020)
-            #print("setting: {}".format(setting))
+            print("commanding {}".format(setting))
+            self.servoFifo.write("0, {}\n".format(setting))
+            self.servoFifo.write("1, {}\n".format(setting))
+            self.servoFifo.write("2, {}\n".format(setting))
+            self.servoFifo.write("3, {}\n".format(setting))
+            self.servoFifo.write("4, {}\n".format(setting))
+            self.servoFifo.write("5, {}\n".format(setting))
+            self.servoFifo.write("6, {}\n".format(setting))
+            self.servoFifo.write("7, {}\n".format(setting))
+            self.servoFifo.write("8, {}\n".format(setting))
+            self.servoFifo.write("9, {}\n".format(setting))
             setting += delta
             tick += 1
         self.position = toTimeUSec
+        self.servoFifo.close()
+        #print(self.fdServoFifo)
+        #os.fsync(self.servoFifo.fileno())
+        self.servoFifo = open("/dev/servo_fifo", "w")
         print("storing new position: {}".format(toTimeUSec))
         self.servoBusy = False
         return(toTimeUSec)
